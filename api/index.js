fcf14d5c1d069fba298e081b45797a6b261fa3f8
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-04-29 19:42:24 +0200

    ADD/UPDATE Context Login logout

--- api/index.js
+++ api/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import usersRoutes from "./routes/users.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
 
 const port = 8800;
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", postRoutes);
 app.use("/api/posts", usersRoutes);
